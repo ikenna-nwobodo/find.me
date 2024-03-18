f7ca9168e09498223ae8a321839c669acd9d37b1
Nwobodo Ikenna <nikenna06@gmail.com>
2024-03-18 13:16:42 +0100

    Update the navigation bar and home page layout, add a lime and light purple background to the stats section, and hide the light purple background on smaller screens.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App bg-white min-h-screen flex flex-col relative items-center">
       <Nav />
-      <div className="bg-primary w-full p-4 flex flex-col relative overflow-hidden items-center min-h-[80vh] md:min-h-screen">
+      <div className="bg-primary w-full p-4 flex flex-col relative overflow-hidden items-center min-h-[90vh] md:min-h-screen">
         <Home />
         <div className="bg-home absolute h-full w-full top-0 bg-contain opacity-20"></div>
       </div>
